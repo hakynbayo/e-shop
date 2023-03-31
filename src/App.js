10ea60f98df4ab1cce2801e5374f0b1fee4979f6
hakynbayo <akinbayo.akinwande@decagon.dev>
2023-03-31 17:46:05 +0100

    boiler-plates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
+const [first, setFirst]= useState("bayo")
   return (
     <div>
       <h1>Hello World!!!</h1>
---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Admin.scss'
+
+function Admin() {
+  return (
+    <div>Admin</div>
+  )
+}
+
+export default Admin
