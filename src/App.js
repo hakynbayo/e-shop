eb68f46edf11a7ebdb39f7792a7b7c0bee915995
hakynbayo <akinbayo.akinwande@decagon.dev>
2023-04-02 23:50:51 +0100

    nav-link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { useState } from "react";
-import "./App.scss";
+// import { useState } from "react";
+// import "./App.scss";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+// Pages
+import {Home, Contact} from "./pages/index";
+
+// Components
+import {Header, Footer} from "./components/index";
+
 
 function App() {
-const [first, setFirst]= useState("bayo")
+// const [first, setFirst]= useState("bayo")
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+      <Header/>
+      
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
